82d20f75bbf1fe4d38de45bad68756c4ffe4a557
Fabian Filipe <fabfilipelino@gmail.com>
2024-09-12 13:12:16 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import Login from './pages/Login';
+import './App.css';  // Importar o CSS global
+import AulasExperimentais from './pages/AulasExperimentais';
+import Mensalidades from './pages/Mensalidades';
+import Pagamentos from './pages/Pagamentos';
+import Relatorios from './pages/Relatorios';
+import UsersPermissoes from './pages/UsersPermissoes';
+import Modalidades from './pages/Modalidades';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/aulasexperimentais" element={<AulasExperimentais />} />
+        <Route path="/mensalidades" element={<Mensalidades />} />
+        <Route path="/modalidades" element={<Modalidades />} />
+        <Route path="/pagamentos" element={<Pagamentos />} />
+        <Route path="/relatorios" element={<Relatorios />} />
+        <Route path="/userpermissoes" element={<UsersPermissoes />} />
+      </Routes>
+    </Router>
   );
 }
 
